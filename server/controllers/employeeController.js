2139decdb0c37e5e640d5c51b893f7a76fb35956
bisratlike <bisratlike@gmail.com>
2024-05-16 17:57:31 +0300

    create employee

--- server/controllers/employeeController.js
+++ server/controllers/employeeController.js
@@ -1,175 +1,169 @@
 const Employee = require("../models/Employee");
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 const bcrypt = require("bcrypt");
-const { validationResult } = require('express-validator');
+const { validationResult } = require("express-validator");
 const jwt = require("jsonwebtoken");
-require('dotenv').config();
+require("dotenv").config();
 
-
-class employeeController{
-    
-
-    static async createEmployee(req, res){
+class employeeController {
+  static async createEmployee(req, res) {
     // Validate user input
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ errors: errors.array() });
     }
 
     try {
-        // Hash the password
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+      // Hash the password
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
-        // Create a new user record
-        const employee = new Employee({
+      // Create a new user record
+      const employee = new Employee({
         fullName: req.body.fullName,
         email: req.body.email,
         password: hashedPassword,
         role: req.body.role,
         types: req.body.types,
-        companyId:req.body.types,
-        });
+        companyId: req.body.companyId,
+      });
 
-        // Save the new employee record
-        await employee.save();
+      // Save the new employee record
+      await employee.save();
 
-        // Send an email containing the password to the new employee
-        const transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: "deluxgemstore@gmail.com",
-                pass: "jrnc eviz hxpr vmyb ",
-            }
-            });
+      // Send an email containing the password to the new employee
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: "deluxgemstore@gmail.com",
+          pass: "jrnc eviz hxpr vmyb ",
+        },
+      });
 
-        const mailOptions = {
+      const mailOptions = {
         from: 'deluxgemstore@gmail.com"',
         to: req.body.email,
-        subject: 'Welcome to Our Company',
-        text: `Dear ${req.body.fullName},\n\nWelcome to our company! Your account has been successfully created. Your password is: ${req.body.password}`,
-        };
+        subject: "Welcome to Our Company",
+        text: `Dear ${req.body.fullName},\n\nWelcome to our company! Your account has been successfully created.your role is ${req.body.role} Your password is: ${req.body.password}`,
+      };
 
-        transporter.sendMail(mailOptions, (error, info) => {
+      transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
-            console.error(`Error occurred while sending email: ${error.message}`);
+          console.error(`Error occurred while sending email: ${error.message}`);
         } else {
-            console.log(`Email sent: ${info.response}`);
+          console.log(`Email sent: ${info.response}`);
         }
-        });
+      });
 
-        return res.status(201).json({ message: 'Employee created successfully' });
+      return res.status(201).json({ message: "Employee created successfully" });
     } catch (error) {
-        console.error(`Error occurred while creating an employee: ${error.message}`);
-        return res.status(500).json({ error: "An error occurred while creating an employee." });
+      console.error(
+        `Error occurred while creating an employee: ${error.message}`
+      );
+      return res
+        .status(500)
+        .json({ error: "An error occurred while creating an employee." });
     }
-    };
+  }
 
+  //one employee
+  static async getOneEmployee(req, res) {
+    try {
+      const token = req.header("Authorization").split(" ")[1];
+      const decodedToken = jwt.verify(token, process.env.jwtWebTokenKey);
+      const id = decodedToken.employeeId;
+      const employee = await Employee.findById(id);
 
-    //one employee
-    static async getOneEmployee(req, res) {
-      
-      try {
-        const token = req.header("Authorization").split(" ")[1];
-        const decodedToken = jwt.verify(token, process.env.jwtWebTokenKey);
-        const id = decodedToken.employeeId;
-        const employee = await Employee.findById(id);
-  
-        if (!employee) {
-          return res.status(404).json({ error: "employee not found" });
-        }
-  
-        return res.status(200).json(employee);
-      } catch (error) {
-        console.error(
-          `Error occured while gettting a employee: ${error.message}`
-        );
-        return res
-          .status(500)
-          .json({ error: "An error occured while getting a employee" });
+      if (!employee) {
+        return res.status(404).json({ error: "employee not found" });
       }
+
+      return res.status(200).json(employee);
+    } catch (error) {
+      console.error(
+        `Error occured while gettting a employee: ${error.message}`
+      );
+      return res
+        .status(500)
+        .json({ error: "An error occured while getting a employee" });
     }
-  
+  }
 
+  static async getAllEmployees(req, res) {
+    try {
+      const employees = await Employee.find();
+      return res.status(200).json(employees);
+    } catch (error) {
+      console.error(
+        `Error occurred while retrieving employees: ${error.message}`
+      );
+      return res
+        .status(500)
+        .json({ error: "An error occurred while retrieving employees." });
+    }
+  }
 
+  static async updateEmployee(req, res) {
+    const { id } = req.params; // Employee ID
 
-    static async getAllEmployees(req, res) {
-        try {
-          const employees = await Employee.find();
-          return res.status(200).json(employees);
-        } catch (error) {
-          console.error(`Error occurred while retrieving employees: ${error.message}`);
-          return res.status(500).json({ error: "An error occurred while retrieving employees." });
-        }
+    try {
+      // Find the employee by ID
+      const existingEmployee = await Employee.findById(id);
+
+      if (!existingEmployee) {
+        return res.status(404).json({ error: "Employee not found." });
       }
-      
-     
 
-      
-    static async updateEmployee(req, res) {
-        const { id } = req.params; // Employee ID
-      
-        try {
-          // Find the employee by ID
-          const existingEmployee = await Employee.findById(id);
-      
-          if (!existingEmployee) {
-            return res.status(404).json({ error: "Employee not found." });
-          }
-          
-          
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-          
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
-          // Update the employee fields
-          existingEmployee.fullName = req.body.fullName || existingEmployee.fullName;
-          existingEmployee.email = req.body.email || existingEmployee.email;
-          existingEmployee.password = hashedPassword || existingEmployee.password;
-          existingEmployee.phoneNumber = req.body.phoneNumber || existingEmployee.phoneNumber;
-          existingEmployee.role = req.body.role || existingEmployee.role;
-          existingEmployee.types = req.body.types || existingEmployee.types;
-          existingEmployee.types = req.body.types || existingEmployee.companyId;
-      
-          // Save the updated employee
-          const updatedEmployee = await existingEmployee.save();
-      
-          // Send the updated password to the updated email
-          
+      // Update the employee fields
+      existingEmployee.fullName =
+        req.body.fullName || existingEmployee.fullName;
+      existingEmployee.email = req.body.email || existingEmployee.email;
+      existingEmployee.password = hashedPassword || existingEmployee.password;
+      existingEmployee.phoneNumber =
+        req.body.phoneNumber || existingEmployee.phoneNumber;
+      existingEmployee.role = req.body.role || existingEmployee.role;
+      existingEmployee.types = req.body.types || existingEmployee.types;
+      existingEmployee.types = req.body.types || existingEmployee.companyId;
 
-            const transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: "deluxgemstore@gmail.com",
-                pass: "jrnc eviz hxpr vmyb ",
-            }
-            });
+      // Save the updated employee
+      const updatedEmployee = await existingEmployee.save();
 
-      
-          const mailOptions = {
-            from: 'deluxgemstore@gmail.com',
-            to: updatedEmployee.email,
-            subject: 'Your password has been updated',
-            text: `Dear ${updatedEmployee.fullName},\n\nYour password has been updated to: ${req.body.password}`,
-          };
-      
-          transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-              console.error(`Error occurred while sending email: ${error.message}`);
-            } else {
-              console.log(`Email sent: ${info.response}`);
-            }
-          });
-      
-          return res.status(200).json(updatedEmployee);
-        } catch (error) {
-          console.error(`Error occurred while updating an employee: ${error.message}`);
-          return res.status(500).json({ error: "An error occurred while updating an employee." });
+      // Send the updated password to the updated email
+
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: "deluxgemstore@gmail.com",
+          pass: "jrnc eviz hxpr vmyb ",
+        },
+      });
+
+      const mailOptions = {
+        from: "deluxgemstore@gmail.com",
+        to: updatedEmployee.email,
+        subject: "Your password has been updated",
+        text: `Dear ${updatedEmployee.fullName},\n\nYour password has been updated to: ${req.body.password}`,
+      };
+
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.error(`Error occurred while sending email: ${error.message}`);
+        } else {
+          console.log(`Email sent: ${info.response}`);
         }
-      }
-      
+      });
 
-
+      return res.status(200).json(updatedEmployee);
+    } catch (error) {
+      console.error(
+        `Error occurred while updating an employee: ${error.message}`
+      );
+      return res
+        .status(500)
+        .json({ error: "An error occurred while updating an employee." });
+    }
+  }
 }
 
 module.exports = employeeController;
-
-  
--- server/routes/employeeRoutes.js
+++ server/routes/employeeRoutes.js
@@ -3,8 +3,20 @@ const router = express.Router();
 const employeeController = require("../controllers/employeeController");
 const authMiddleware = require("../middleware/authMiddleware");
 
-router.post("/createEmployee",authMiddleware.authorize("manager"||"ceo"),employeeController.createEmployee);
-router.get("/allEmployee", authMiddleware.authorize("manager"||"ceo"), employeeController.getAllEmployees);
-router.put("/updateEmployee/:id",authMiddleware.authorize("manager"||"ceo"),employeeController.updateEmployee);
-router.get("/oneEmployee",employeeController.getOneEmployee);
+router.post(
+  "/createEmployee",
+  authMiddleware.authorize("ceo"),
+  employeeController.createEmployee
+);
+router.get(
+  "/allEmployee",
+  authMiddleware.authorize("manager" || "ceo"),
+  employeeController.getAllEmployees
+);
+router.put(
+  "/updateEmployee/:id",
+  authMiddleware.authorize("manager" || "ceo"),
+  employeeController.updateEmployee
+);
+router.get("/oneEmployee", employeeController.getOneEmployee);
 module.exports = router;
